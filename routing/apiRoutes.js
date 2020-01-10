3ea8f83d7f31d4018b6bdb38a1590d3b8a02aff1
Veger Pena <vegerpena@ymail.com>
2020-01-09 21:28:29 -0500

    added route for home page

---
+++ routing/apiRoutes.js
@@ -0,0 +1,56 @@
+// Dependencies
+var http = require("http");
+var fs = require("fs");
+
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+  var path = req.url;
+
+  switch (path) {
+  case "/thanks":
+    return renderThankYouPage(req, res);
+  default:
+    return renderWelcomePage(req, res);
+  }
+}
+
+function renderWelcomePage(req, res) {
+  fs.readFile(__dirname + "/home.html", function(err, data) {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
+    }
+    else {     
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    }
+  });
+}
+
+function renderThankYouPage(req, res) {
+  // Saving the request posted data as a variable.
+  var requestData = "";
+
+  var myHTML =
+    "<html><head><title>Hello Noder!</title></head><body><h1>Oops, I didn't get any data</h1></body></html>";
+
+  // When the server receives data, it will add it to requestData.
+  req.on("data", function(data) {
+    requestData += data;
+  });
+
+  // When the request has ended...
+  req.on("end", function() {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end();
+    console.log(requestData);
+  });
+}
+
+// Starts our server.
+server.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
